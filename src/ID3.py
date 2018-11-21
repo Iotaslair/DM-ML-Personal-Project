e1ca072f0215ddf0b93224ac2b3a469d71bdec41
William Pembleton <william.pembleton17@ncf.edu>
2018-11-21 10:41:09 -0500

    Changes to stuff I did and ID3 code (gave up)
    Am now using WEKA

--- src/ID3.py
+++ src/ID3.py
@@ -3,32 +3,32 @@
 from sklearn import tree
 import pandas as pan
 import numpy as np
-import graphviz
-import subprocess
+#import graphviz
+#import subprocess
 
-df = pan.read_csv("PreProcessedDataCartNoNeighbourhood.csv")
+df = pan.read_csv("StackOverflow semicolons done satisfaction done 500.csv")
 #df = pan.read_csv("test.csv")
 #df.drop(columns=[""])
 #print(df)
 print("Df imported")
+print()
 
-Target = df[df.columns[9]]
-#Features = df[("PatientID", "Gender", "AppointmentDay", "Age", "Neighbourhood",
-#            "Scholarship", "Hipertension", "Diabetes", "Alcoholism", "Handcap", 
-#            "SMS_received")]
-Features = df[list(df.columns[:9])]
+Target = df[df.columns[32]]
 
-dt = tree.DecisionTreeClassifier(min_samples_split=1000)
+columns = list(df.columns)
+
+columns.remove('JobSatisfaction')
+Features = df[columns]
+
+dt = tree.DecisionTreeClassifier(max_depth=3)#min_samples_split=1000)
 dt = dt.fit(Features, Target)
 #print(dt)
 #with open("dt.dot", 'w') as f:
 print("Exporting...")
-dot_data = tree.export_graphviz(dt, out_file=None,
-                                feature_names=list(df.columns[:9].values),
-                                class_names = ["Yes", "No"])
+#dot_data = tree.export_graphviz(dt, out_file=None,feature_names=list(df.columns[:9].values), class_names = ["Yes", "No"])
 #print(dot_data)
-graph = graphviz.Source(dot_data) 
-graph.render("CARTOut")
+#graph = graphviz.Source(dot_data) 
+#graph.render("ID3")
 print("Done")
 #exit()
 #    command = ["dot", "-Tpng", "dt.dot", "-o", "dt.png"]
